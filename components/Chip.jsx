6b71ebde10e3276a42a81a750f9d02cb29c8c8a9
Enkhsaikhan <ch.enhsaihan@gmail.com>
2024-01-18 19:43:57 +0800

    Chip - small update

--- components/Chip.jsx
+++ components/Chip.jsx
@@ -1,8 +1,10 @@
 export function Chip(props) {
   const { label } = props;
   return (
-    <div className="bg-gray-600 text-white inline-block px-4 py-2 rounded-xl">
-      {label}
+    <div className="flex justify-center">
+      <div className="bg-gray-600 text-white flex justify-center items-center w-28 px-4 py-2 rounded-xl">
+        {label}
+      </div>
     </div>
   );
 }
