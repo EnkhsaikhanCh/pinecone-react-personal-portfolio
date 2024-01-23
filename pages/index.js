c0b3bacd18d1e55e738baa036715b3c9965e8821
Enkhsaikhan <ch.enhsaihan@gmail.com>
2024-01-23 20:53:32 +0800

    New component - Container

--- pages/index.js
+++ pages/index.js
@@ -2,14 +2,21 @@ import { Header } from "@/components/Header";
 import { Hero } from "@/components/Hero";
 import { About } from "@/components/About";
 import { Skills } from "@/components/Skills";
+import { Container } from "@/components/Container";
+import { Experience } from "@/components/Experience";
+import { Work } from "@/components/Work";
 
 export default function Home() {
   return (
     <div>
       <Header />
-      <Hero />
-      <About />
-      <Skills />
+      <Container>
+        <Hero />
+        <About />
+        <Skills />
+        <Experience />
+        <Work />
+      </Container>
     </div>
   );
 }
