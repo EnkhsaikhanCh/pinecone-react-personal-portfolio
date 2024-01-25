b75a91086049c7c83cabb90c958d2deb5284031d
Enkhsaikhan <ch.enhsaihan@gmail.com>
2024-01-25 19:21:56 +0800

    New component - "Footer"

---
+++ components/Footer.jsx
@@ -0,0 +1,10 @@
+export function Footer() {
+  return (
+    <div className="bg-gray-50 py-[24px]">
+      <p className="flex justify-center gap-1 text-gray-600">
+        <img src="/iconSVG/icon-copyright.svg" alt="" />
+        2023 | Designed and coded with ❤️️ by Max Shah
+      </p>
+    </div>
+  );
+}
