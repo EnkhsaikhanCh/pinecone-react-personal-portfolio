805c49838717a6fd760e9cebc95b91719e671525
Enkhsaikhan <ch.enhsaihan@gmail.com>
2024-01-26 21:14:35 +0800

    Updated - icons
    - Move to Components icons folder
    - Change svg to jsx
    - Added dark mode color

--- components/icons/Menu_icon.jsx
+++ components/icons/Menu_icon.jsx
@@ -6,24 +6,25 @@ export function MenuIcon() {
       viewBox="0 0 36 36"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
+      className="dark:text-[#D1D5DB]"
     >
       <path
         d="M10 18H26"
-        stroke="#4B5563"
+        stroke="currentColor"
         strokeWidth="2"
         strokeLinecap="round"
         strokeLinejoin="round"
       />
       <path
         d="M10 12H26"
-        stroke="#4B5563"
+        stroke="currentColor"
         strokeWidth="2"
         strokeLinecap="round"
         strokeLinejoin="round"
       />
       <path
         d="M10 24H26"
-        stroke="#4B5563"
+        stroke="currentColor"
         strokeWidth="2"
         strokeLinecap="round"
         strokeLinejoin="round"
